4fd8acb8654b0f60c594c4545a4cd5b75256a8e9
Santiago Rios Valero <riossanval@gmail.com>
2024-11-01 14:02:01 -0500

    [ADD] dms font and persistent auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,19 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter/material.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:stacked_services/stacked_services.dart';
 import 'package:tots_test/app/app.bottomsheets.dart';
 import 'package:tots_test/app/app.dialogs.dart';
 import 'package:tots_test/app/app.locator.dart';
 import 'package:tots_test/app/app.router.dart';
-import 'package:stacked_services/stacked_services.dart';
-
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await setupLocator();
   setupDialogUi();
   setupBottomSheetUi();
+  await dotenv.load();
+  await GetStorage.init();
   runApp(const MainApp());
 }
 
@@ -19,12 +23,25 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: Routes.loginView,
+      theme: ThemeData(
+        textTheme: GoogleFonts.dmSansTextTheme(
+          Theme.of(context).textTheme,
+        ),
+      ),
+      initialRoute: _setInitialRoute(),
       onGenerateRoute: StackedRouter().onGenerateRoute,
       navigatorKey: StackedService.navigatorKey,
       navigatorObservers: [
         StackedService.routeObserver,
       ],
     );
+  }
+
+  String _setInitialRoute() {
+    final box = GetStorage();
+    if (box.hasData('token')) {
+      return Routes.homeView;
+    }
+    return Routes.loginView;
   }
 }
