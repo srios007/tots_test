8e3cfcb35035476a6bd504378de81133ce0b704f
Santiago Rios Valero <riossanval@gmail.com>
2024-10-31 22:47:08 -0500

    [UPDATE] change primary color values in app_colors.dart

--- lib/ui/common/app_colors.dart
+++ lib/ui/common/app_colors.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 
-const Color kcPrimaryColor = Color(0xFF9600FF);
-const Color kcPrimaryColorDark = Color(0xFF300151);
+const Color kcPrimaryColor = Color(0xFFE4F353);
+const Color kcPrimaryColorDark = Color(0xFF0D1111);
 const Color kcDarkGreyColor = Color(0xFF1A1B1E);
 const Color kcMediumGrey = Color(0xFF474A54);
-const Color kcLightGrey = Color.fromARGB(255, 187, 187, 187);
-const Color kcVeryLightGrey = Color(0xFFE3E3E3);
-const Color kcBackgroundColor = kcDarkGreyColor;
