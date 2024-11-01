877458d2075251ed785695664d283753487d7e88
Santiago Rios Valero <riossanval@gmail.com>
2024-11-01 14:05:20 -0500

    [ADD] login

--- lib/ui/views/login/login_view.dart
+++ lib/ui/views/login/login_view.dart
@@ -1,6 +1,12 @@
-import 'package:flutter/material.dart';
-import 'package:stacked/stacked.dart';
+import 'dart:ui';
 
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:stacked/stacked.dart';
+import 'package:tots_test/ui/common/common.dart';
+
+import '../../../widgets/widgets.dart';
+import '../../ui.dart';
 import 'login_viewmodel.dart';
 
 class LoginView extends StackedView<LoginViewModel> {
@@ -14,14 +20,82 @@ class LoginView extends StackedView<LoginViewModel> {
   ) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.surface,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text('Login View'),
-          ElevatedButton(
-            onPressed: viewModel.navigateToHomeView,
-            child: const Text('Navigate to Home View'),
-          ),
+      body: CustomScrollView(
+        slivers: [
+          SliverFillRemaining(
+            hasScrollBody: false,
+            child: Stack(
+              children: [
+                Positioned(
+                  top: 0,
+                  right: 0,
+                  child: Image.asset(
+                    ImageRoutes.blurTop,
+                    width: Get.width * 0.8,
+                  ),
+                ),
+                Positioned(
+                  bottom: 0,
+                  child: Image.asset(
+                    ImageRoutes.blurBottom,
+                    width: Get.width,
+                  ),
+                ),
+                Positioned(
+                  left: 0,
+                  top: Get.height / 2 - (Get.width * 0.4) / 2,
+                  child: Image.asset(
+                    ImageRoutes.blurLeft,
+                    width: Get.width * 0.4,
+                  ),
+                ),
+                SizedBox(
+                  width: Get.width,
+                  child: Form(
+                    key: viewModel.formKey,
+                    child: Column(
+                      children: [
+                        const Spacer(flex: 7),
+                        const SizedBox(height: 20),
+                        Image.asset(
+                          ImageRoutes.logo,
+                          width: 200,
+                        ),
+                        const SizedBox(height: 20),
+                        const Spacer(flex: 2),
+                        Text(
+                          'LOG IN',
+                          style: TextStyle(
+                            color: Palette.black.withOpacity(0.85),
+                            fontSize: 12,
+                            fontWeight: FontWeight.bold,
+                            letterSpacing: 2.5,
+                          ),
+                        ),
+                        const Spacer(),
+                        const SizedBox(height: 20),
+                        EmailInput(
+                          textEditingController: viewModel.emailController,
+                        ),
+                        const SizedBox(height: 30),
+                        PasswordInput(
+                          textEditingController: viewModel.passwordController,
+                        ),
+                        const SizedBox(height: 50),
+                        CustomButton(
+                          buttonText: 'LOG IN',
+                          isLoading: viewModel.isLoading,
+                          onPressed: viewModel.login,
+                        ),
+                        const SizedBox(height: 20),
+                        const Spacer(flex: 7),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
         ],
       ),
     );
