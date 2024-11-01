aa2b9894d82b3eedddf422d8396b543227b75bf6
Santiago Rios Valero <riossanval@gmail.com>
2024-11-01 14:03:47 -0500

    [ADD] bareel files

---
+++ lib/services/services.dart
@@ -0,0 +1,3 @@
+export 'http/dio_exceptions.dart';
+export 'http/dio_service.dart';
+export 'login/login_service.dart';
---
+++ lib/ui/ui.dart
@@ -0,0 +1,16 @@
+export 'bottom_sheets/notice/notice_sheet.dart';
+export 'bottom_sheets/notice/notice_sheet_model.dart';
+export 'common/api_endoints.dart';
+export 'common/app_strings.dart';
+export 'common/common.dart';
+export 'common/image_routes.dart';
+export 'common/palette.dart';
+export 'common/text_styles.dart';
+export 'common/ui_helpers.dart';
+export 'dialogs/info_alert/info_alert_dialog.dart';
+export 'dialogs/info_alert/info_alert_dialog_model.dart';
+export 'snackbars/custom_snackbars.dart';
+export 'views/home/home_view.dart';
+export 'views/home/home_viewmodel.dart';
+export 'views/login/login_view.dart';
+export 'views/login/login_viewmodel.dart';
