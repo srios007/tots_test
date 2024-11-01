bf5244210303af5b5b695ad9432d9816213e6ef1
Santiago Rios Valero <riossanval@gmail.com>
2024-11-01 14:05:13 -0500

    [ADD] color palette

--- lib/ui/common/palette.dart
+++ lib/ui/common/palette.dart
@@ -8,7 +8,9 @@ class Palette {
   Palette._internal();
 
   static final _instance = Palette._internal();
-  static const primary = Color(0xFFE4F353);
+  static const mainColor = Color(0xFFE4F353);
   static const black = Color(0xFF0D1111);
-  static const grey = Color(0xFF474A54);
+  static const blackTitle = Color(0xFF161B14);
+  static const grey = Color(0xFFA3AAB1);
+  static const white = Color(0xFFFFFFFF);
 }
