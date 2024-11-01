12eae9f8ab18235fca76e7ad387ae6a50fc3e3d5
Santiago Rios Valero <riossanval@gmail.com>
2024-11-01 14:04:01 -0500

    [ADD] login

--- lib/services/login/login_service.dart
+++ lib/services/login/login_service.dart
@@ -1,1 +1,18 @@
-class LoginService {}
+import 'dart:developer';
+
+import '../../ui/common/common.dart';
+import '../services.dart';
+
+class LoginService {
+  Future<String?> login({required String email, required String password}) async {
+    final response = await DioService.instance.post(ApiEndpoints.login, data: {
+      'email': email,
+      'password': password,
+    });
+    log('Response: $response');
+    if (response != null) {
+      return response['access_token'];
+    }
+    return null;
+  }
+}
--- lib/ui/views/login/login_viewmodel.dart
+++ lib/ui/views/login/login_viewmodel.dart
@@ -1,13 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
+import 'package:tots_test/ui/ui.dart';
 
 import '../../../app/app.locator.dart';
 import '../../../app/app.router.dart';
+import '../../../services/services.dart';
 
 class LoginViewModel extends BaseViewModel {
   final _navigationService = locator<NavigationService>();
+  final _loginService = locator<LoginService>();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+
+  RxBool isLoading = false.obs;
 
   void navigateToHomeView() {
-    _navigationService.navigateTo(Routes.homeView);
+    _navigationService.replaceWith(Routes.homeView);
+  }
+
+  void login() async {
+    if (!formKey.currentState!.validate()) return;
+
+    isLoading.value = true;
+
+    final response = await _loginService.login(
+      email: emailController.text,
+      password: passwordController.text,
+    );
+
+    isLoading.value = false;
+
+    if (response != null) {
+      final box = GetStorage();
+      box.write('token', response);
+      navigateToHomeView();
+    } else {
+      CustomSnackBars.showErrorSnackBar(
+        message: 'No es posible iniciar sesión, verifica tus credenciales',
+      );
+    }
   }
 }
