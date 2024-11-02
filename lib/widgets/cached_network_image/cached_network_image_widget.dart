7a40245706354a902ae1296e040895f3abb7341b
Santiago Rios Valero <riossanval@gmail.com>
2024-11-01 19:39:42 -0500

    [FIX] cached network image

--- lib/widgets/cached_network_image/cached_network_image_widget.dart
+++ lib/widgets/cached_network_image/cached_network_image_widget.dart
@@ -1,19 +1,18 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
+import '../../ui/ui.dart';
 import '../widgets.dart';
 
 class CachedNetworkImageWidget extends StatelessWidget {
   const CachedNetworkImageWidget({
     super.key,
     required this.imageUrl,
-    this.errorWidget,
     this.width = 60,
     this.height = 60,
   });
 
   final String imageUrl;
-  final Widget? errorWidget;
   final double? width;
   final double? height;
 
@@ -28,22 +27,7 @@ class CachedNetworkImageWidget extends StatelessWidget {
         return const LoadingWidget();
       },
       errorWidget: (context, url, error) {
-        return _buildErrorWidget(context, imageUrl.replaceAll('jpg', 'png'));
-      },
-    );
-  }
-
-  Widget _buildErrorWidget(BuildContext context, String fallbackUrl) {
-    return CachedNetworkImage(
-      imageUrl: fallbackUrl,
-      fit: BoxFit.cover,
-      width: width,
-      height: height,
-      progressIndicatorBuilder: (context, url, downloadProgress) {
-        return const LoadingWidget();
-      },
-      errorWidget: (context, url, error) {
-        return errorWidget ?? _defaultErrorWidget();
+        return _defaultErrorWidget();
       },
     );
   }
@@ -56,7 +40,13 @@ class CachedNetworkImageWidget extends StatelessWidget {
         color: Colors.white,
         shape: BoxShape.circle,
       ),
-      child: Image.asset('assets/images/default.jpg'),
+      child: Container(
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          border: Border.all(color: Palette.black),
+        ),
+        child: const Icon(Icons.person),
+      ),
     );
   }
 }
