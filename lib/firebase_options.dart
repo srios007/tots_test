cd2c5c038c2c4f63ee5de21e8c2779d3b7a2d9a5
Santiago Rios Valero <riossanval@gmail.com>
2024-11-04 18:39:52 -0500

    [ADD] secrets in .env

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -3,6 +3,7 @@
 import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
 import 'package:flutter/foundation.dart'
     show defaultTargetPlatform, kIsWeb, TargetPlatform;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 /// Default [FirebaseOptions] for use with your Firebase apps.
 ///
@@ -49,20 +50,20 @@ class DefaultFirebaseOptions {
     }
   }
 
-  static const FirebaseOptions android = FirebaseOptions(
-    apiKey: 'AIzaSyDyXim_SCjBPUlL1LuBgUL1iRbchJAr0Rs',
-    appId: '1:344530313038:android:38e2df8c255fd723ba481f',
-    messagingSenderId: '344530313038',
-    projectId: 'tots-9cf1a',
-    storageBucket: 'tots-9cf1a.firebasestorage.app',
+  static FirebaseOptions get android => FirebaseOptions(
+    apiKey: dotenv.env['ANDROID_API_KEY']!,
+    appId: dotenv.env['ANDROID_APP_ID']!,
+    messagingSenderId: dotenv.env['ANDROID_MESSAGING_SENDER_ID']!,
+    projectId: dotenv.env['ANDROID_PROJECT_ID']!,
+    storageBucket: dotenv.env['ANDROID_STORAGE_BUCKET']!,
   );
 
-  static const FirebaseOptions ios = FirebaseOptions(
-    apiKey: 'AIzaSyB-aMy7NYQZxFIgDJ7cz9gYB3e5nGd01cg',
-    appId: '1:344530313038:ios:6ce538f2878791f8ba481f',
-    messagingSenderId: '344530313038',
-    projectId: 'tots-9cf1a',
-    storageBucket: 'tots-9cf1a.firebasestorage.app',
-    iosBundleId: 'com.example.totsTest',
+  static FirebaseOptions get ios => FirebaseOptions(
+    apiKey: dotenv.env['IOS_API_KEY']!,
+    appId: dotenv.env['IOS_APP_ID']!,
+    messagingSenderId: dotenv.env['IOS_MESSAGING_SENDER_ID']!,
+    projectId: dotenv.env['IOS_PROJECT_ID']!,
+    storageBucket: dotenv.env['IOS_STORAGE_BUCKET']!,
+    iosBundleId: dotenv.env['IOS_BUNDLE_ID']!,
   );
 }
